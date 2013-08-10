7dc836a8c5451c0497179b9be0412448d7344b1e
fukang <fukang@maildfstw.com>
2013-08-10 11:37:55 +0800

    add cookie and user agent.

--- HTMLFetchAndParse/HTMLFetchAndParse/HTMLFetcher.cs
+++ HTMLFetchAndParse/HTMLFetchAndParse/HTMLFetcher.cs
@@ -14,20 +14,38 @@ namespace HTMLFetchAndParse
         //for load page
         HttpWebRequest myHttpWebRequest;
         HttpWebResponse myHttpWebResponse;
-        public CookieContainer cookies = new CookieContainer();
+        public CookieContainer Cookies { set; get; }
+        public String UserAgent { set; get; }
 
+        public HTMLFetcher()
+        {
+            Cookies = new CookieContainer();
+            UserAgent = "Mozilla/5.0 (Windows NT 5.1; rv:7.0.1) Gecko/20100101 Firefox/7.0.1";
+        }
 
+        public void AddCookie(String cookieString, String domain)
+        {
+            var cookieCollection = new CookieCollection();
+            var cookies = cookieString.Trim().Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var cookie in cookies)
+            {
+                var datas = cookie.Trim().Split(new char[] { '=' }, 2);
+                cookieCollection.Add(new Cookie(datas[0], datas[1]));
+            }
+            Cookies.Add(new Uri(domain), cookieCollection); 
+            
+        }
         public void LoadPage(string uri, string filename, string encodingName)
         {
             try
             {
                 myHttpWebRequest = (HttpWebRequest)WebRequest.Create(new Uri(uri));
-                myHttpWebRequest.CookieContainer = cookies;
+                myHttpWebRequest.CookieContainer = Cookies;
 
 
                 myHttpWebRequest.ContentType = "application/x-www-form-urlencoded";
                 myHttpWebRequest.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,*//*;q=0.8";
-                myHttpWebRequest.UserAgent = "Mozilla/5.0 (Windows NT 5.1; rv:7.0.1) Gecko/20100101 Firefox/7.0.1";
+                myHttpWebRequest.UserAgent = UserAgent;
 
 
                 myHttpWebResponse = (HttpWebResponse)myHttpWebRequest.GetResponse();
@@ -51,7 +69,8 @@ namespace HTMLFetchAndParse
 //                Thread.Sleep(1200000);
 
                 myHttpWebRequest = (HttpWebRequest)WebRequest.Create(new Uri(uri));
-                myHttpWebRequest.CookieContainer = cookies;
+                myHttpWebRequest.CookieContainer = Cookies;
+                myHttpWebRequest.UserAgent = UserAgent;
 
                 myHttpWebResponse = (HttpWebResponse)myHttpWebRequest.GetResponse();
                 using (var myResponseStream = myHttpWebResponse.GetResponseStream())
@@ -73,12 +92,12 @@ namespace HTMLFetchAndParse
             try
             {
                 myHttpWebRequest = (HttpWebRequest)WebRequest.Create(new Uri(uri));
-                myHttpWebRequest.CookieContainer = cookies;
+                myHttpWebRequest.CookieContainer = Cookies;
 
 
                 myHttpWebRequest.ContentType = "application/x-www-form-urlencoded";
                 myHttpWebRequest.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,*//*;q=0.8";
-                myHttpWebRequest.UserAgent = "Mozilla/5.0 (Windows NT 5.1; rv:7.0.1) Gecko/20100101 Firefox/7.0.1";
+                myHttpWebRequest.UserAgent = UserAgent;
 
 
                 myHttpWebResponse = (HttpWebResponse)myHttpWebRequest.GetResponse();
@@ -101,7 +120,8 @@ namespace HTMLFetchAndParse
                 //                Thread.Sleep(1200000);
 
                 myHttpWebRequest = (HttpWebRequest)WebRequest.Create(new Uri(uri));
-                myHttpWebRequest.CookieContainer = cookies;
+                myHttpWebRequest.CookieContainer = Cookies;
+                myHttpWebRequest.UserAgent = UserAgent;
 
                 myHttpWebResponse = (HttpWebResponse)myHttpWebRequest.GetResponse();
                 using (var myResponseStream = myHttpWebResponse.GetResponseStream())
@@ -120,13 +140,13 @@ namespace HTMLFetchAndParse
             try
             {
                 myHttpWebRequest = (HttpWebRequest)WebRequest.Create(new Uri(uri));
-                myHttpWebRequest.CookieContainer = cookies;
+                myHttpWebRequest.CookieContainer = Cookies;
 
                 myHttpWebRequest.Method = "POST";
                 myHttpWebRequest.KeepAlive = true;
                 myHttpWebRequest.ContentType = "application/x-www-form-urlencoded";
                 myHttpWebRequest.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,*//*;q=0.8";
-                myHttpWebRequest.UserAgent = "Mozilla/5.0 (Windows NT 5.1; rv:7.0.1) Gecko/20100101 Firefox/7.0.1";
+                myHttpWebRequest.UserAgent = UserAgent;
 
                 using (var requestStream = myHttpWebRequest.GetRequestStream())
                 using (var writer = new StreamWriter(requestStream))
@@ -153,7 +173,8 @@ namespace HTMLFetchAndParse
                 Console.WriteLine("Uri : " + uri);
 
                 myHttpWebRequest = (HttpWebRequest)WebRequest.Create(new Uri(uri));
-                myHttpWebRequest.CookieContainer = cookies;
+                myHttpWebRequest.CookieContainer = Cookies;
+                myHttpWebRequest.UserAgent = UserAgent;
 
                 myHttpWebRequest.Method = "POST";
                 myHttpWebRequest.KeepAlive = true;
